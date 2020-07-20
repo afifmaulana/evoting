7f3ac3cfbff4095ab232110874e117ffd7b182c4
Afif Maulana Iskandar <afifmaulanaiskandar@gmail.com>
2020-07-20 22:56:59 +0700

    Send Password User

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -32,5 +33,11 @@ class AppServiceProvider extends ServiceProvider
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
         date_default_timezone_set('Asia/Jakarta');
+
+        $throttleRate = config('mail.throttleToMessagesPerMin');
+        if ($throttleRate) {
+            $throttlerPlugin = new \Swift_Plugins_ThrottlerPlugin($throttleRate, \Swift_Plugins_ThrottlerPlugin::MESSAGES_PER_MINUTE);
+            Mail::getSwiftMailer()->registerPlugin($throttlerPlugin);
+        }
     }
 }
