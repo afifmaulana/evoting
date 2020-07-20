f4fe240fd2b385c9a53686fff8e1a4acdeccda43
Afif Maulana Iskandar <afifmaulanaiskandar@gmail.com>
2020-07-20 23:22:25 +0700

    Send Password User

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -34,10 +34,10 @@ class AppServiceProvider extends ServiceProvider
         Carbon::setLocale('id');
         date_default_timezone_set('Asia/Jakarta');
 
-        $throttleRate = config('mail.throttleToMessagesPerMin');
-        if ($throttleRate) {
-            $throttlerPlugin = new \Swift_Plugins_ThrottlerPlugin($throttleRate, \Swift_Plugins_ThrottlerPlugin::MESSAGES_PER_MINUTE);
-            Mail::getSwiftMailer()->registerPlugin($throttlerPlugin);
-        }
+//        $throttleRate = config('mail.throttleToMessagesPerMin');
+//        if ($throttleRate) {
+//            $throttlerPlugin = new \Swift_Plugins_ThrottlerPlugin($throttleRate, \Swift_Plugins_ThrottlerPlugin::MESSAGES_PER_MINUTE);
+//            Mail::getSwiftMailer()->registerPlugin($throttlerPlugin);
+//        }
     }
 }
--- config/mail.php
+++ config/mail.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'throttleToMessagesPerMin' => env('MAIL_THROTTLE_TO_MESSAGES_PER_MIN', null), //https://mailtrap.io has a rate limit of 2 emails/sec per inbox, but consider being even more conservative.
+    //'throttleToMessagesPerMin' => env('MAIL_THROTTLE_TO_MESSAGES_PER_MIN', null), //https://mailtrap.io has a rate limit of 2 emails/sec per inbox, but consider being even more conservative.
 
 
     /*
