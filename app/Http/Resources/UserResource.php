514742e37171f3fce6aee3b8d979ebaa4376891d
Afif Maulana Iskandar <afifmaulanaiskandar@gmail.com>
2020-07-30 07:53:16 +0700

    pemilihan

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'                    => $this->id,
+            'id_adminsekolah'       => (int)$this->id_adminsekolah,
+            'nis'                   => $this->nis,
+            'nama_siswa'            => $this->nama_siswa,
+            'email'                 => $this->email,
+            'kelas'                 => $this->kelas,
+            'email'                 => $this->email
+        ];
+    }
+}
