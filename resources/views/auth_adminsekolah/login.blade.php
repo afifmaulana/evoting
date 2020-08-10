b333ec118282d5daeaaaeee4d585e23d2851b987
Afif Maulana Iskandar <afifmaulanaiskandar@gmail.com>
2020-08-10 12:08:44 +0700

    revisi

--- resources/views/auth_adminsekolah/login.blade.php
+++ resources/views/auth_adminsekolah/login.blade.php
@@ -23,6 +23,7 @@
 <div class="container-scroller">
     <div class="container-fluid page-body-wrapper full-page-wrapper">
         <div class="content-wrapper d-flex align-items-center auth auth-bg-1 theme-one">
+            <h2 class="text-center mb-4">Login Admin Sekolah</h2>
             <div class="row w-100 mx-auto">
                 <div class="col-lg-4 mx-auto">
                     <div class="auto-form-wrapper">
